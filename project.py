9a313961edf7f1e7683e591f08c1b57b407448ce
Abubakr Sharif <Sharif@MacBook-Pro-6.local>
2019-05-10 12:30:26 -0500

    Update project.py

--- project.py
+++ project.py
@@ -5,7 +5,7 @@ It processes the file, cleans it up,
 breaks it into sentences line by line, then into words to compare to the other 
 two files, 't' which is the positive words file, and 'u' which is the negative words file.
 Then prints out how many times a Postive, Negative, and Neutral word was in the txt.
-Neutral in this case is just any words that arent in the pos or neg files.
+Neutral in this case is just any words that aren't in the pos or neg files.
 
 """
 
@@ -45,9 +45,6 @@ def sentimentanalysis(s,t,u):
 	print("Positive:", Positive)
 	print("Negative:", Negative)
 	print("Neutral :", Neutral)
-
-
-
 
 def main():
 	sentimentanalysis("thehoundofthebaskervilles.txt","positivesentimentwords.txt","negativesentimentwords.txt")
