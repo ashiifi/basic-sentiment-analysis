04906a7c733742c9ab7fc74b190539305ca0b143
ashiifi <ashiifi@gmail.com>
2019-09-15 20:15:09 -0500

    Small Update

--- sentimentanalysis.py
+++ sentimentanalysis.py
@@ -4,7 +4,7 @@ sentimentanalysis(s,t,u) takes in a txt file 's'.
 It processes the file, cleans it up,
 breaks it into sentences line by line, then into words to compare to the other
 two files, 't' which is the positive words file, and 'u' which is the negative words file.
-Then prints out how many times a Postive, Negative, and Neutral word was in the txt.
+Then prints out how many times a Postive, Negative, and Neutral word was in the text.
 Neutral in this case is just any words that aren't in the pos or neg files.
 """
 
